027e155987fd8dc8e843a3706ad3e756f971d4ef
hariharanragothaman <hariharanragothaman@gmail.com>
2020-07-28 01:48:52 -0400

    hashmap basics in C++

---
+++ refresh_cpp/hash_map.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <unordered_map>
+using namespace std;
+
+/*
+Key Points to remember:
+1. Remember unordered_map faster than map since it doesn't care about order
+
+Operations to be done:
+1. Putting values into hashmap
+2. Deleting stuff from hashmap
+3. Sorting the hashmap by key
+4. Sorting the hashmap by value
+5. Sorting the hashmap by key and then value - viceversa.
+6. Merge 2 hashmaps
+7. Print stuff from hashmap
+8. Checking if a key exists in a hashmap
+*/
+
+int main()
+{
+    cout << "Hashmap Reference" << endl;
+    vector<char> arr = {'a', 'g', 'd', 'c', 'e', 'b', 'f'};
+    
+    // 1. Putting stuff into the hashmap
+    unordered_map<char, int> hmap;
+    int idx = 0;
+    for(auto& elem: arr)
+    {
+        hmap[elem] = idx++;
+    }
+
+    // Checking if the hashmap contains a specific key
+    if ( hmap.find('e') != hmap.end()  )
+    {
+        cout << "Yaay the key exists\n";
+    }
+
+    // Sorting the hashmap by key
+    // One of the easiest ways to put the unordered_map into a map
+    map<char, int> ordered(hmap.begin(), hmap.end());
+    for(auto it = ordered.begin(); it != ordered.end(); ++it)
+    {
+        cout << "Key: " << it->first << " Value: " << it->second <<endl;
+    }
+
+}
