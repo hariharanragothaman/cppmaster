eba175e27b9eeba98c19621b9c56dd218e6b3a8c
hariharanragothaman <hariharanragothaman@gmail.com>
2020-08-22 15:24:34 -0400

    stringstream revision

---
+++ refresh_cpp/learning_stringstream.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <unordered_map>
+using namespace std;
+
+// Basically this the equivalent of a str.split('delim') in python.
+
+int countwords(string str)
+{
+    stringstream s(str);
+    string word;
+
+    int count = 0;
+    while(s >> word)
+    {
+        count++;
+    }
+
+    return count;
+}
+
+// Now this is the equivalent of a counter in python
+void printFrequency(string str)
+{
+    unordered_map<string, int> hashmap;
+    stringstream ss(str);
+    string word;
+
+    while (ss >> word)
+    {
+        hashmap[word]++;
+    }
+
+    for (auto it = hashmap.begin(); it !=hashmap.end(); it++)
+    {
+        cout << it->first << "->" << it->second << "\n";
+    }
+    
+}
+
+
+
+int main()
+{
+    string s = "leetcode for life";
+    cout <<"The number of words is:" << countwords(s) << endl;
+    printFrequency(s);
+    return 0;
+}
