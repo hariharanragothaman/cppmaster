0827defec7c9b973c87eec0fc95e2bc0711cef59
hariharanragothaman <hariharanragothaman@gmail.com>
2020-09-26 01:19:46 -0400

    A few more vector polishes

---
+++ refresh_cpp/arrays/vector_clear.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void display(vector<int>& nums)
+{
+    for(auto it: nums)
+        cout << it << endl;
+}
+
+int main()
+{
+    vector<int> input = {1, 2, 3, 4, 5};
+    display(input);
+    // Erases all the elements in the array
+    input.clear();
+    display(input);
+}
