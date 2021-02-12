428004bad680a9e71c658f9fc8639ad62d9af22e
hariharanragothaman <hariharanragothaman@gmail.com>
2021-02-12 18:02:06 -0500

    Sorting a hashmap by value

---
+++ refresh_cpp/hashmaps/sort_hashmap_by_value.cpp
@@ -0,0 +1,34 @@
+#include "../../headers.h"
+
+
+#include "../../headers.h"
+
+// Modify the functor here to for reverse=True functionality 
+bool sort_by_value(const pair<char, int> &a, const pair<char, int> &b)
+{
+    return (a.second < b.second);
+}
+
+int main()
+{
+    vector<char> arr = {'a', 'g', 'd', 'c', 'e', 'b', 'f', 'a', 'c', 'e'};
+    unordered_map<char, int> hmap;
+    for(auto& elem: arr)
+    {
+        hmap[elem] += 1;
+    }
+
+    vector<pair<char, int>> vec;
+    for(auto& elem: hmap)
+    {
+        vec.push_back(make_pair(elem.first, elem.second));
+    }
+    sort(vec.begin(), vec.end(), sort_by_value);
+
+    cout << "Printing pairs after sorting by value" << endl;
+    for(auto& it: vec)
+    {
+        cout << it.first << it.second << endl;
+    }
+
+}
