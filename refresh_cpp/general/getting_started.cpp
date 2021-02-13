458efe7c1aebdcf2b1f68e78070beba8f5359c01
hariharanragothaman <hariharanragothaman@gmail.com>
2021-02-12 20:55:49 -0500

    Adding more content to cheat-sheet; using lambda's / predicates

--- refresh_cpp/general/getting_started.cpp
+++ refresh_cpp/general/getting_started.cpp
@@ -1,10 +1,4 @@
-#include <iostream>
-#include <algorithm>
-#include <unordered_map>
-#include <vector>
-#include <bits/stdc++.h>
-
-using namespace std;
+#include "../../headers.h"
 
 int main()
 {
@@ -12,29 +6,11 @@ int main()
 
     // This initializes an array in C++ and we print it
     int input [] = {2, 7, 11, 15};
-
     for(int n = 0; n < 4; n++)
-    {
         cout << input[n];
-    }
-
-    cout << "\n";
+    cout << endl;
 
     vector<int> vinput = {1, 2, 3, 4};
-    // This is one style of writing a for-loop
-    for(auto i : vinput)
-    {
-        cout << i ;
-    }
-    cout << "\n";
-
-    // This is probably the closest enumerate
-    int index = 0;
-    for(auto& elem : vinput)
-    {
-        cout << elem << "\t" <<index << "\n";
-    }
-
     // Another traditional way of writing
     for(size_t i = 0; i < vinput.size(); ++i)
     {
@@ -43,33 +19,24 @@ int main()
 
     std::cout<<"Playing with Hashmaps" << "\n";
 
-    // Put elements into the unorded - hashmap
+    // Put elements into the unordered - hashmap
     unordered_map<int, int> hmap;
     int loc = 0;
     for(auto& elem : vinput)
-    {
         hmap[elem] = loc++;
-    }
 
     // print the hashmap
     for (auto& kv : hmap)
-    {
         cout<< "Key: " << kv.first << " Value:" << kv.second << "\n";
-    }
 
-    cout << "Figuring out ordered hashmap\n";
-    //Put the elements into an ordered hashmp
+    // Put the elements into a raw map
     map<int, int> ordered;
     int idx = 0;
     for(auto& elem : vinput)
-    {
         ordered[elem] = idx++;
-    }
 
-    // Printing the ordered map
+    // Printing the raw map
     for(auto &n : ordered)
-    {
         cout << n.first <<":" << n.second << "\n";
-    }
 
 }
