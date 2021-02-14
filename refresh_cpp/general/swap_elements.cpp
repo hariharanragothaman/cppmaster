1fe3f1aa17f9b57692c5bca942658393b7bf3578
hariharanragothaman <hariharanragothaman@gmail.com>
2021-02-13 22:12:42 -0500

    Swap elements , Replace elements (with lambdas ) and directly using replace()

---
+++ refresh_cpp/general/swap_elements.cpp
@@ -0,0 +1,13 @@
+#include "../../headers.h"
+
+/*
+ *  swap() returns void - swaps the given 2 values
+ */
+
+int main()
+{
+    int a = 7;
+    int b = 343;
+    swap(a, b);
+    cout << a << endl << b << endl;
+}
