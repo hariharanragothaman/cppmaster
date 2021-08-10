d0d1d9c615575760dbf0757253a2ac15a975916c
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2021-08-10 19:50:11 -0400

    Some overall cleanup

---
+++ math/get_neighbours2D.cpp
@@ -0,0 +1,9 @@
+#include "../headers.h"
+
+
+
+int main()
+{
+    vector<vector<int>> arr = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    /* To parse the matrix and get the neighbours of each of the elements */
+}
