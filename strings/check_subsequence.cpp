19b3d661cf3096744bfc8910b73b517ab1a22141
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2021-08-10 18:50:16 -0400

    Adding recipe to check if it's a subsequence or not

---
+++ strings/check_subsequence.cpp
@@ -0,0 +1,23 @@
+#include "../headers.h"
+
+// Check if s is a subsequence of t
+bool isSubsequence(string s, string t)
+{
+    int m = s.size();
+    int n = t.size();
+    int j = 0;
+    for(int i=0; i<n; i++)
+    {
+        if(s[j]==t[i])
+            j++;
+    }
+    return j==m;
+}
+
+int main()
+{
+    string s = "elrld";
+    string t = "HelloWorld";
+    cout << isSubsequence(s, t);
+    return 0;
+}
