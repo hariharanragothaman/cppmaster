60f0fde177bb87f726a7167333153d47e96ab522
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2021-09-10 15:45:58 -0400

    Adding policy based dataStructures in g++

---
+++ datastructures/orderedSet/orderedSet.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Hariharan Ragothaman on 9/10/21.
+//
+
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp>
+#include <functional> // for less
+#include <iostream>
+using namespace __gnu_pbds;
+using namespace std;
+
+
+int main()
+{
+    return 0;
+}
