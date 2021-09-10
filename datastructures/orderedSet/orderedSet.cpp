96bf7a376da9a6bba3b4d8dc166488b5d6029c08
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2021-09-10 17:06:59 -0400

    Adding some driver code for orderedSet

--- datastructures/orderedSet/orderedSet.cpp
+++ datastructures/orderedSet/orderedSet.cpp
@@ -4,13 +4,23 @@
 
 #include <ext/pb_ds/assoc_container.hpp> // Common file
 #include <ext/pb_ds/tree_policy.hpp>
-#include <functional> // for less
-#include <iostream>
 using namespace __gnu_pbds;
+
+#include <functional> // for less
+
+#include <iostream>
 using namespace std;
 
-
+typedef tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update> new_data_set;
 int main()
 {
+    new_data_set p;
+    p.insert(5);
+    p.insert(2);
+    p.insert(6);
+    p.insert(4);
+    cout << "The value at 3rd index :: " << *p.find_by_order(3) << endl;
+    cout << "The index of number 6:: " << p.order_of_key(6) << endl;
+    cout << "The index of number seven :: " << p.order_of_key(7) << endl;
     return 0;
 }
