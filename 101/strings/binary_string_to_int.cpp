63faa235db3d7cfdff65ca979e9c60efad2966f5
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2021-11-10 13:36:36 -0500

    Binary to string conversion for large integers

---
+++ 101/strings/binary_string_to_int.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Hariharan Ragothaman on 11/10/21.
+//
+
+#include "bits/stdc++.h"
+using namespace std;
+
+int main()
+{
+    string s = "111";
+    int ans = stoi(s, nullptr, 2);
+    cout << "The answer is:" << ans << endl;
+}
