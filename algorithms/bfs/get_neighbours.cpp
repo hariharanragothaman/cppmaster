6e6260cea297c351b934d408baf70c436079e389
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2021-11-29 02:12:30 -0500

    Adding BFS, DFS, matrix templates

---
+++ algorithms/bfs/get_neighbours.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Hariharan Ragothaman on 11/29/21.
+//
+
+#include "bits/stdc++.h"
+using namespace std;
+
+vector<vector<int>> get_neighbours(int& r, int& c, int& R, int& C)
+{
+    vector<vector<int>> directions = {{r+1, c}, {r, c+1}, {r-1, c}, {r, c-1}};
+    vector<vector<int>> nei;
+    for(auto c: directions)
+    {
+        if(0<= c[0] && c[0]< R && c[1] >= 0 && c[1] < C)
+            nei.push_back({c[0], c[1]});
+    }
+    return nei;
+}
