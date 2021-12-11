3fb9433ae0b46252f5f57e9c92e4bdff69f265c9
Hariharan Ragothaman <hariharanragothaman@Hariharans-MacBook-Pro.local>
2021-12-11 07:00:29 -0500

    Given a number convert to binary string

---
+++ strings/convert_number_to_binary_string.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Hariharan Ragothaman on 12/11/21.
+//
+
+#include "bits/stdc++.h"
+using namespace std;
+
+int main()
+{
+    int n = 7;
+    string str = bitset<8>(n).to_string();
+    cout << str;
+}
