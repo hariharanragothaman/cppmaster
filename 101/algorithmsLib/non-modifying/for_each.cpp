8195ec5128058147367baeab1bb020d94b37c2e4
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2022-11-15 12:07:07 -0500

    Adding some helpful comments about map

--- 101/algorithmsLib/non-modifying/for_each.cpp
+++ 101/algorithmsLib/non-modifying/for_each.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <map>
 
 using namespace std; 
 
@@ -38,5 +39,42 @@ int main()
     cout << endl;
 
     cout << s.sum << endl;
+
+    /**
+     *  Now let's try for_each with map()
+     *  This is not possible, and will result in an an error.
+     *  The problem with changing the key of a std::map (or the value of a std::set)
+        Contrary to sequence containers such as std::vector, std::map and std::set offers 2 guarantees:
+            they maintain their elements in sorted order,
+            they ensure their elements are unique (except for std::multimap and std::multiset).
+        If you don’t need those invariants, you can just use a std::vector and be done with it.
+        However in the case of a map, the sorted order is convenient to make find the value associated to a key in logarithmic complexity.
+        To maintain these invariants, the containers std::map and std::set need to keep some control about the relative positions of their values inside of the collection.
+        If you just go about and modify a value by using an iterator, like in the example above, the container won’t be notified. This will make its structure inconsistent and break the invariants.
+    */
+    map<int, int> H;
+    for(int i=0; i<5; i++)
+    {
+        H[i]++;
+    }
+
+    cout << "Printing the map" << endl;
+    for(auto c: H)
+    {
+        cout << c.first << " " << c.second << endl;
+    }
+
+    cout << "Modifying the map using lambda" << endl;
+    for_each(H.begin(), H.end(), [](pair<int, int> c)
+    {
+        c.first++;
+    });
+
+    cout << "Printing the map after update" << endl;
+    for(auto c: H)
+    {
+        cout << c.first << " " << c.second << endl;
+    }
+
     return 0;
 }
