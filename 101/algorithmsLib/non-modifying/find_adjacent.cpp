999348c7feac6827ada32dc4ab66e85ecdc0c7cd
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2022-11-15 21:29:26 -0500

    Adding routine for find_adjacent

--- 101/algorithmsLib/non-modifying/find_adjacent.cpp
+++ 101/algorithmsLib/non-modifying/find_adjacent.cpp
@@ -1,3 +1,31 @@
-//
-// Created by Hariharan Ragothaman on 11/15/22.
-//
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> A = {1, 2, 3, 4, 4, 5, 6, 7, 7,  8,  9, 10};
+    auto it = adjacent_find(A.begin(), A.end());
+    if(it == A.end())
+    {
+        cout << "No matching adjacent elements" << endl;
+    }
+    else
+    {
+        cout << "The first adjacent pair of equal elements "
+        << distance(A.begin(),it) << " , " << *it << endl;
+    }
+
+    auto it2 = adjacent_find(A.begin(), A.end(), greater<int>());
+    if(it2 == A.end())
+    {
+        cout << "The vector is sorted in ascending order" << endl;
+    }
+    else
+    {
+        cout << "The last element in non-decreasing subsequence " << distance(A.begin(), it2) << " "
+        << *it2 << endl;
+    }
+    return 0;
+}
