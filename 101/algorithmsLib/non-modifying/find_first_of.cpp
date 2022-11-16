c1db771126010f0bff964662abe2ec24a0ba4292
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2022-11-16 04:34:44 -0500

    Adding find_end and find_first_of

--- 101/algorithmsLib/non-modifying/find_first_of.cpp
+++ 101/algorithmsLib/non-modifying/find_first_of.cpp
@@ -1,20 +1,17 @@
 /*
- *  To find the last occurance of t in s
+ * find first occurrence of t in s
  */
 
 #include <iostream>
 #include <string>
 #include <algorithm>
-
 using namespace std;
 
 int main()
 {
     string s = "oreo_cookies";
-    string t = "oo";
-    // Find the last occurrence of t in s
-    string::iterator it = find_end(s.begin() ,s.end(), t.begin(), t.end());
+    string t = "re";
+    string::iterator it = find_first_of(s.begin(), s.end(), t.begin(), t.end());
     cout << "The index is: " << it - s.begin();
     return 0;
-
 }
